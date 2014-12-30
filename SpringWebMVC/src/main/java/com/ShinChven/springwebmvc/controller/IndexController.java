d0846cef622a38eec1693da551f0e5e7b66c7b24
ShinChven <shinchven@gmail.com>
2014-12-30 14:15:30 +0800

    Thymeleaf usage

--- SpringWebMVC/src/main/java/com/ShinChven/springwebmvc/controller/IndexController.java
+++ SpringWebMVC/src/main/java/com/ShinChven/springwebmvc/controller/IndexController.java
@@ -1,6 +1,5 @@
 package com.ShinChven.springwebmvc.controller;
 
-import com.ShinChven.springwebmvc.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,12 +10,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class IndexController {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello(User user) {
-        return "index";
+    public String hello() {
+        return "login";
     }
 
-    @RequestMapping(value = "/hello", method = RequestMethod.POST)
-    public String postHello(User user) {
-        return "redirect:/hello.do";
-    }
+
 }
