46043056394407a06f961956f06ab0a17efc22c6
ShinChven <shinchven@gmail.com>
2014-12-30 11:51:48 +0800

    加入Thymeleaf

--- SpringWebMVC/src/main/java/com/ShinChven/springwebmvc/controller/IndexController.java
+++ SpringWebMVC/src/main/java/com/ShinChven/springwebmvc/controller/IndexController.java
@@ -2,18 +2,21 @@ package com.ShinChven.springwebmvc.controller;
 
 import com.ShinChven.springwebmvc.entity.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by ShinChven on 2014/12/29.
  */
 @Controller
 public class IndexController {
-    @RequestMapping(value = "/hello",method = RequestMethod.GET)
-    public String hello(User user){
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello(User user) {
         return "index";
+    }
+
+    @RequestMapping(value = "/hello", method = RequestMethod.POST)
+    public String postHello(User user) {
+        return "redirect:/hello.do";
     }
 }
--- SpringWebMVC/src/main/java/com/ShinChven/springwebmvc/entity/User.java
+++ SpringWebMVC/src/main/java/com/ShinChven/springwebmvc/entity/User.java
@@ -4,4 +4,13 @@ package com.ShinChven.springwebmvc.entity;
  * Created by ShinChven on 2014/12/30.
  */
 public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
